458540b4ec7e3154086efd8e94720bafc19e92fc
LUCAS\monto <lucasmontoby@gmail.com>
2024-07-23 19:24:05 -0300

    fin-json

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- router/productoRouter.js
+++ router/productoRouter.js
@@ -1,13 +1,24 @@
 const router = require("express").Router();
 const productoController = require("../controller/productosController");
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 router.get("/", productoController.list);
 
 router.get("/create", productoController.create);
-router.post("/create", productoController.stock);
+router.post("/create", upload.single('imagen'), productoController.stock);
 
 router.get("/:id/edit", productoController.edit);
-router.put("/:id", productoController.update);
+router.put("/:id", upload.single('imagen'), productoController.update);
 
 router.get("/:id/delete", productoController.delete);
 router.delete("/:id", productoController.destroy);
